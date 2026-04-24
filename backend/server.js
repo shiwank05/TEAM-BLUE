5d888b7e240211815f63049f09ca927b44dd57e8
shiwank <shiwank07@gmail.com>
2026-04-24 14:42:52 +0530

    Fix MongoDB URI to use env variable

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ if (!fs.existsSync('./uploads')) {
 }
 
 // MongoDB Connection
-mongoose.connect('mongodb://localhost:27017/teammanagement')
+mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log('MongoDB connected successfully'))
 .catch((err) => console.error('MongoDB connection error:', err));
 
